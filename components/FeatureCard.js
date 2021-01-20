cb4feb3fe9ed6be37f1ddea03332d764d8a0be7b
Jake Harper <jake@krunchy.net>
2021-01-20 00:26:58 -0800

    fixes sequential descending issues

--- components/FeatureCard.js
+++ components/FeatureCard.js
@@ -1,6 +1,6 @@
 export default function FeatureCard(props) {
     return <div className={`py-4 px-6 bg-${props.color}-200 rounded-md max-w-sm m-2`}>
-        <h4 className={`font-bold text-${props.color}-900 text-xl mb-2`}>{props.title}</h4>
+        <h3 className={`font-bold text-${props.color}-900 text-xl mb-2`}>{props.title}</h3>
         <p className={`text-${props.color}-900`}>{props.content}</p>
     </div>
 }
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <Container>
         <div className="flex flex-col justify-center items-start max-w-4xl mx-auto mb-16">
             <div className="text-center mb-24">
-                <h3 className="text-blueGray-500 dark:text-white uppercase tracking-widest">Safe2Ski</h3>
+                <p className="text-blueGray-500 dark:text-white uppercase tracking-widest">Safe2Ski</p>
                 <h1 className="font-extrabold text-7xl tracking-tight mb-8 text-blueGray-900 dark:text-white">
                     The days of unsafe journeys and blindsiding conditions have finally <span className="text-blue-500">come to an end.</span>⛷️
                 </h1>
@@ -22,8 +22,8 @@ export default function Home() {
                 <h3 className="font-bold text-blueGray-700 text-3xl mb-4">The Passes We're Currently<br/> Watching</h3>
                 <div className="flex shadow-lg rounded-md flex-wrap">
                     <div className="w-full lg:w-1/2 py-6 px-8">
-                        <h1 className="text-lg font-bold text-blueGray-900">Snoqualmie Pass</h1>
-                        <h1 className="text-lg text-blueGray-700">Snoqualmie Pass is a mountain pass that carries Interstate 90 through the Cascade Range in the U.S. state of Washington. The pass summit is at an elevation of 3,015 feet, on the county line between Kittitas County and King County. <Link href="/destination/summit-at-snoqualmie"><a className="text-blue-600"><small>View Today's Conditions</small></a></Link></h1>
+                        <h2 className="text-lg font-bold text-blueGray-900">Snoqualmie Pass</h2>
+                        <h2 className="text-lg text-blueGray-700">Snoqualmie Pass is a mountain pass that carries Interstate 90 through the Cascade Range in the U.S. state of Washington. The pass summit is at an elevation of 3,015 feet, on the county line between Kittitas County and King County. <Link href="/destination/summit-at-snoqualmie"><a className="text-blue-600"><small>View Today's Conditions</small></a></Link></h2>
                     </div>
                     <div className="w-full lg:w-1/2" >
                         <img className="rounded-md" alt="snoqualmie pass" src="https://beautifulwashington.com/images/summit-ski-area/summit-ski-area-2.jpg"/>
