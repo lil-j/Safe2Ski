350ec92d9437cf06ab4e2d5179cff7c20f8677ce
Jake Harper <jake@krunchy.net>
2021-01-23 19:29:04 -0800

    Note in DOM

--- components/Note.js
+++ components/Note.js
@@ -1,7 +1,7 @@
 function Note(props) {
     console.log(props.positive)
     return <div className="p-2 w-full lg:w-1/2">
-        <div className={`"px-3 py-3 rounded-md bg-${props.positive ? "green" : "red"}-300 text-${props.positive ? "green" : "red"}-800"`}>
+        <div className={`"px-3 py-3 rounded-md bg-${props.positive ? "green" : "red"}-200 text-${props.positive ? "green" : "red"}-900"`}>
             {props.note}
         </div>
     </div>
