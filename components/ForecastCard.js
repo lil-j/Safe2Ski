dc639fa090f48503a98a3ff4ca847b0bdbe4471a
Jake Harper <jake@krunchy.net>
2021-01-24 15:11:35 -0800

    Weather forecast adds Show MOre

--- components/ForecastCard.js
+++ components/ForecastCard.js
@@ -1,3 +1,5 @@
+import ShowMoreText from 'react-show-more-text';
+
 function ForecastCard(props) {
     const { forecast } = props;
     if (!forecast.name.includes("Night")){
@@ -5,7 +7,7 @@ function ForecastCard(props) {
                 <div className="h-full border border-blueGray-200 border-2 rounded-md py-4 px-2">
                 <h4 className="text-blueGray-900">{forecast.name}</h4>
                 <h5 className="text-blueGray-800 font-bold text-5xl">{forecast.temperature}° <small>F</small></h5>
-                <p className="text-blueGray-600">{forecast.detailedForecast}</p>
+                <ShowMoreText className="text-blueGray-600" anchorClass='text-blue-700'>{forecast.detailedForecast}</ShowMoreText>
             </div>
         </div>
     }
