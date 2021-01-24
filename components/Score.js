2c4025e36f784911924bc48942bf3806b1bbd8fc
Jake Harper <jake@krunchy.net>
2021-01-23 17:12:04 -0800

    Animate Score

--- components/Score.js
+++ components/Score.js
@@ -20,6 +20,27 @@ function Score(props) {
                 <CircularProgressbar
                     value={value}
                     text={props.SafetyScore}
+                    styles={buildStyles({
+                        // Rotation of path and trail, in number of turns (0-1)
+                        rotation: 0.25,
+
+                        // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                        strokeLinecap: 'butt',
+
+                        // Text size
+                        textSize: '16px',
+
+                        // How long animation takes to go from one percentage to another, in seconds
+                        pathTransitionDuration: 1,
+
+                        // Can specify path transition in more detail, or remove it entirely
+                        // pathTransition: 'none',
+
+                        // Colors
+                        pathColor: `rgba(94, 152, 199, ${props.SafetyScore / 100})`,
+                        trailColor: '#d6d6d6',
+                        backgroundColor: '#3e98c7',
+                    })}
                 />
             );
         }}
