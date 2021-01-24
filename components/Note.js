cf161d7874126101ce419a3ae2fcf6dcef4d9ead
Jake Harper <jake@krunchy.net>
2021-01-23 19:44:08 -0800

    Notes - adds icon

--- components/Note.js
+++ components/Note.js
@@ -1,8 +1,15 @@
+import { CheckCircleOutline, XCircleOutline } from "heroicons-react";
 function Note(props) {
     console.log(props.positive)
     return <div className="p-2 w-full lg:w-1/2">
         <div className={`"px-3 py-3 rounded-md bg-${props.positive ? "green" : "red"}-200 text-${props.positive ? "green" : "red"}-900`}>
-            {props.note}
+            <div className="flex">
+                {props.positive ?<CheckCircleOutline className="flex-none mx-2"/> : <XCircleOutline className="flex-none mx-2"/> }
+                <div>
+                    {props.note}
+                </div>
+            </div>
+
         </div>
     </div>
 }
