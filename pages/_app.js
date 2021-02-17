dadf55f5558baffa88d7740618d0994151d2734f
Jake Harper <jake@krunchy.net>
2021-02-16 20:44:06 -0800

    loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import "../styles/globals.css"
 import 'react-circular-progressbar/dist/styles.css';
+import NextNprogress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+      <NextNprogress
+          color="#29D"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height="3"
+      />
+      <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
